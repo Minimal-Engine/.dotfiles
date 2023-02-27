26b36193c998f21d271b81c67effe0f2c85564c3
Dawid Lukasz Rzeszutek-Shibuya <dawidrzeszutek@gmail.com>
2023-02-27 09:16:15 +0100

    setup-laptop.sh erweitert

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
 
 echo "This will setup my Laptop"
+# Eingabe zusätzlicher Paketquellen
+
+## Google-Chrome
+
+## Virtualbox
+
+## Steam
+
+# Installation aller meiner benötigten Pakete aus den regulären und 
+# zusätzlichenPaketquellen (Siehe auch Paketlisten.txt)
+
+# Installation von GIT und Erzeugung von SSH-Keys
+
+# Ablage von SSH-Key auf Github
+
+# Cloning der GIT-Repositories
+
+## Eigene Dotfiles clonen
+
+## Clonen des VIM-Paketmangers und OhMyZsh
+
+## Clonen der Autoergänzung 
+
+## Stowing meiner Einstellungen für VIM, ZSH, GIT, TMUX, SSH 
