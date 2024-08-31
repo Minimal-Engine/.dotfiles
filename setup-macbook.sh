2f7e23dae9577b768bf511850e15d82642370a75
Dawid Lukasz Rzeszutek-Shibuya <dawidrzeszutek@gmail.com>
2024-08-31 18:07:01 +0800

    setup-macbook.sh überarbeitet

---
+++ setup-macbook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## system-settings
+
+
+## setup git and pull my .dotfiles
+
+
+
+## install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
