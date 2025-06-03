2f2ac8271ff84ea90d57c0f6401bc337515ab8e7
Dawid Lukasz Rzeszutek-Shibuya <d.rzeszutek@icloud.com>
2025-06-03 16:14:10 +0800

    Brewfile updated

--- setup-macbook.sh
+++ setup-macbook.sh
@@ -21,6 +21,8 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+
+
 # setup git and pull my .dotfiles
 
 ## stow my settings
@@ -30,3 +32,9 @@
 ## set the jpeg from my brewfiles as centered desktop background
 
 # set some programs to autostart and as permanent into the dock
+
+# setup mpvacious for mpv and Anki
+
+mkdir -p ~/.config/mpv/scripts/
+git clone 'https://github.com/Ajatt-Tools/mpvacious.git' ~/.config/mpv/scripts/subs2srs
+cd ~/.config/mpv/scripts/subs2srs && git pull
