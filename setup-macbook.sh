e79f831d8a355ec27178124b6e178aca503ebe16
Dawid Lukasz Rzeszutek-Shibuya <d.rzeszutek@icloud.com>
2025-06-21 09:53:28 +0800

    setup-macbook.sh modified

--- setup-macbook.sh
+++ setup-macbook.sh
@@ -3,17 +3,19 @@
 # system-settings
 
 ## deactivate SIRI
+defaults write com.apple.assistant.support "Assistant Enabled" -bool false
 
 ## deavtivate STAGE-MANAGER
+defaults write com.apple.WindowManager GloballyEnabled -bool false
 
-## set up hot corners
+## disable all hot-corners
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
 
-### lower left for desktop view
-
-### top left for screen lock
-
-## set up dock, on lower part, autohide, switch off animations.
-
+# autohide the dock
+defaults write com.apple.dock autohide -bool true
 
 # generate a new pair of ssh-keys
 
@@ -21,15 +23,20 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # setup git and pull my .dotfiles
+
+
 
 ## stow my settings
 
 ## install the programms from my brewfile
 
-## set the jpeg from my brewfiles as centered desktop background
+## set the jpeg from my brewfiles as centered desktop background and adapt color settings
+
+
 
 # set some programs to autostart and as permanent into the dock
 
